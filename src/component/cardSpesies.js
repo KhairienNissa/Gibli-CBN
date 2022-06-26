09a7c2d7a877562c56f014282ec8e9c26173ecb7
KhairienNissa <cacakhairien@gmail.com>
2022-06-26 13:16:30 +0700

    update

--- src/component/cardSpesies.js
+++ src/component/cardSpesies.js
@@ -6,8 +6,8 @@ function CardSpesies({item}) {
             <div className="card">
                 <div className="title">{item.name}</div>
                 <div className="desc2">Classification : {item.classification}</div>
-                <div className="desc2">Eye Color : {item.eye_color}</div>
-                <div className="desc2">Hair Color: {item.hair_color}</div>
+                <div className="desc2">Eye Color : {item.eye_colors}</div>
+                <div className="desc2">Hair Color: {item.hair_colors}</div>
             </div>
         </div> 
   );
